896853b915dca86ae29a220a800a477f55213e44
e.sia <e-sia@outlook.com>
2025-04-19 19:57:29 +0100

    reset

--- internal/httpserver/httpserver.go
+++ internal/httpserver/httpserver.go
@@ -1,12 +1,9 @@
 package httpserver
 
 import (
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/swaggo/echo-swagger"
-	"ralts-cms/graph"
 	machinesHttp "ralts-cms/internal/httpserver/handlers/machines"
 	"ralts-cms/internal/machines"
 
@@ -22,22 +19,6 @@ func NewHTTPServer(mh *machinesHttp.Handler, mr *machines.Resolver) *echo.Echo {
 	apiGroup := e.Group("/api")
 	machineGroup := apiGroup.Group("/machines")
 	machineGroup.GET("/:serialnumber", mh.Get)
-
-	graphqlHandler := handler.NewDefaultServer(
-		graph.NewExecutableSchema(
-			graph.Config{Resolvers: &graph.Resolver{MachineResolver: mr}},
-		),
-	)
-	e.POST("/query", func(c echo.Context) error {
-		graphqlHandler.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
-
-	playgroundHandler := playground.Handler("GraphQL", "/query")
-	e.GET("/playground", func(c echo.Context) error {
-		playgroundHandler.ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
 
 	return e
 }
--- pkg/pg/pg.go
+++
@@ -1,5 +0,0 @@
-package pg
-
-import "fmt"
-
-var ErrNotFound = fmt.Errorf("table record does not exist")
